b4cb3cb4e356edaa2a3875d04dad7c8b52cf116c
nihal tekin <nihalpolattekin@gmail.com>
2023-10-03 02:09:40 +0200

    data images

---
+++ src/components/Project/Data.jsx
@@ -0,0 +1,86 @@
+import Work1 from "../../assets/blog-app.gif";
+import Work2 from "../../assets/stockk.gif";
+import Work3 from "../../assets/movieApp.gif";
+import Work4 from "../../assets/recipe.gif";
+import Work5 from "../../assets/countries.gif";
+import Work6 from "../../assets/nba.gif";
+import Work7 from "../../assets/imageGallery.gif";
+
+
+
+
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Blog App",
+    link: "https://blogg-app-ten.vercel.app/",
+    link2: "https://github.com/nihalptekin/Blogg_App",
+    category: "Upper Intermediate",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Stock App",
+    link: "https://stock-app-fullstack-five.vercel.app/",
+    link2: "https://github.com/nihalptekin/StockApp_Fullstack",
+    category: "Upper Intermediate",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Movie App",
+    link: "https://movie-app-react-fay.netlify.app/",
+    link2: "https://github.com/iamfatihay/Movie-app",
+    category: "Upper Intermediate",
+  },
+ 
+  {
+    id: 4,
+    image: Work4,
+    title: "Recipe App",
+    link: "https://recipe-app-project-phi.vercel.app/",
+    link2: "https://github.com/nihalptekin/RecipeAppProject",
+    category: "Intermediate",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Countries App",
+    link: "https://react-router-chi-one.vercel.app/",
+    link2: "https://github.com/nihalptekin/ReactRouter",
+    category: "Intermediate",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "NBA-Legends App",
+    link: "https://nbm-legends.vercel.app/",
+    link2: "https://github.com/nihalptekin/NBM_Legends",
+    category: "Elementary",
+  },
+  {
+    id: 7,
+    image: Work7,
+    title: "Image Gallery",
+    link: "https://001-image-galeri.vercel.app/",
+    link2: "https://github.com/nihalptekin/001-Image-Galeri",
+    category: "Elementary",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "Upper Intermediate",
+  },
+  {
+    name: "Intermediate",
+  },
+  {
+    name: "Elementary",
+  },
+];
